cd90df9aa9dd97ef45af8091056aa5a0c430f359
Rupak Roy <63533688+rupakroy12@users.noreply.github.com>
2020-11-15 12:50:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import random as r
 def send_sms(number, message):
     url = "https://www.fast2sms.com/dev/bulk"
     params = {
-        'authorization': '8iQnCNvMcg69lHoyYfK2arVmOGUBtkAzjLRqPThd70W3bsuDXeUvWTbntzmrO5ARKHVcoQGZDLa49PFp',
+        'authorization': 'your api key goes here',
         'sender_id': 'FSTSMS',
         'message': message,
         'language': 'english',
@@ -17,29 +17,33 @@ def send_sms(number, message):
     response = requests.get(url, params=params)
 
     dict = response.json()
-    print(dict)
-    return dict.get('return')
-
+    
+    return dict
 
 def otpgen():
     otp = ""
     for i in range(4):
         otp += str(r.randint(1, 9))
-    # print("Your One Time Password is " + otp)
     return otp
 
 
 if __name__ == '__main__':
     otp = int(otpgen())
     message = 'Your One time Password is ' + str(otp)
-    number = '9051631521'
+    number = 'your number goes here (for multiple numbers,separate using commas)'
 
-    send_sms(number, message)
+    status = send_sms(number, message)
 
+    if dict.get('return') is False:
+        print("Sorry, Otp could not be sent ! ")
+        
+        print(f'Message : {dict.get("message")}')
     
-    #verify otp
-    n = int(input("Enter OTP : "))
-    if n == int(otp):
-        print("OTP Verification Successful.")
     else:
-        print("Wrong OTP !")
+        print("Otp sent successfully")
+        #verify otp
+        n = int(input("Enter OTP : "))
+        if n == int(otp):
+            print("OTP Verification Successful.")
+        else:
+            print("Wrong OTP !")
